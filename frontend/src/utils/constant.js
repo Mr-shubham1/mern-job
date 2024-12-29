f805649a314c2a36aae177fddfdcdbd1e2aa7158
Mr-shubham1 <shubhamguptajnvc@gmail.com>
2024-12-29 23:07:26 +0530

    Har har mahadev

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,10 +1,8 @@
 export const USER_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/user";
 export const JOB_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/job";
 export const APPLICATIO_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/application";
-<<<<<<< HEAD
 export const COMPANY_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/company";
 
 // http://localhost:8000
-=======
-export const COMPANY_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/company"
->>>>>>> 73c052963f10a52275f48fc70c2707f6a5e6ba4c
+
+
