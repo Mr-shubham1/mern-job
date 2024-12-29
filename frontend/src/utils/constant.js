73c052963f10a52275f48fc70c2707f6a5e6ba4c
Mr-shubham1 <108973912+Mr-shubham1@users.noreply.github.com>
2024-12-29 22:35:54 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATIO_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_END_POINT = "http://localhost:8000/api/v1/company"
+export const USER_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/user";
+export const JOB_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/job";
+export const APPLICATIO_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/application";
+export const COMPANY_END_POINT = "https://mern-job-backend-cnc1.onrender.com/api/v1/company"
