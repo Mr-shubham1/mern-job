88165efcfd3e93e5bc3600e9006b97a7eec38d3a
Mr-shubham1 <shubhamguptajnvc@gmail.com>
2024-12-29 12:20:47 +0530

    3rd commit on 29/12/2024

--- frontend/src/components/admin/Postjobs.jsx
+++ frontend/src/components/admin/Postjobs.jsx
@@ -18,8 +18,9 @@ import { JOB_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import { Loader2 } from "lucide-react";
+import Footer from "../Footer";
 
-const companyArray = [];
+// const companyArray = [];
 
 const Postjobs = () => {
   const [input, setInput] = useState({
@@ -36,27 +37,26 @@ const Postjobs = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const companies = useSelector((store) => store.company.allCompany);
-  // console.log(companies);
+
   const changeEventHandler = (e) => {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
+
   const selecteventHndler = (value) => {
     const selectedCompany = companies.find((company) => {
       return company.name.toLowerCase() === value;
     });
-    // console.log(selectedCompany._id);
     setInput({
       ...input,
       companyId: selectedCompany._id,
     });
-    // console.log(input);
   };
+
   const submitHandler = async (e) => {
     e.preventDefault();
-    // console.log(input);
     try {
       setLoading(true);
       const res = await axios.post(`${JOB_END_POINT}/post`, input, {
@@ -65,8 +65,6 @@ const Postjobs = () => {
         },
         withCredentials: true,
       });
-      // console.log(res);
-
       if (res.data.success) {
         toast.success(res.data.message);
         setLoading(false);
@@ -79,15 +77,41 @@ const Postjobs = () => {
       setLoading(false);
     }
   };
+
   return (
-    <div>
+    <div className="bg-gradient-to-br from-purple-100 via-white to-blue-100">
       <Navbar />
-      <div className="  flex items-center justify-center  my-4">
+      <div className="flex items-center justify-center my-10 px-4 ">
         <form
           onSubmit={submitHandler}
-          className=" max-w-5xl p-4 border border-gray-200 shadow-lg rounded-md"
+          className="max-w-3xl w-full p-6 bg-white border border-gray-200 shadow-lg rounded-md"
         >
-          <div className="grid grid-cols-2 gap-4 ">
+          <h1 className="text-xl font-bold mb-6 text-gray-700 text-center">
+            Post a New Job
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          {companies.length !== 0 && (
+              <div className="col-span-1 sm:col-span-2">
+                <Label>Company <span className="text-red-800 text-lg">*</span> </Label>
+                <Select onValueChange={selecteventHndler}>
+                  <SelectTrigger className="w-full mt-2">
+                    <SelectValue placeholder="Select a Company" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {companies.map((company) => (
+                        <SelectItem
+                          key={company._id}
+                          value={company?.name.toLowerCase()}
+                        >
+                          {company.name}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            )}
             <div>
               <Label>Title</Label>
               <Input
@@ -95,18 +119,18 @@ const Postjobs = () => {
                 name="title"
                 value={input.title}
                 onChange={changeEventHandler}
-                className="w-52 focus-visible:ring-0 focus-visible:ring-offset-0"
+                className=" focus-visible:ring-0 focus-visible:ring-offset-0"
               />
             </div>
             <div>
               <Label>Description</Label>
-              <Input
-                type="text"
+              <textarea
                 name="description"
                 value={input.description}
                 onChange={changeEventHandler}
-                className=" focus-visible:ring-0 focus-visible:ring-offset-0"
-              />
+                rows={2}
+                className="w-full border border-gray-300 rounded-md p-2 focus:ring-purple-500"
+              ></textarea>
             </div>
             <div>
               <Label>Requirements</Label>
@@ -122,10 +146,10 @@ const Postjobs = () => {
             <div>
               <Label>Salary</Label>
               <Input
-                type="text"
+                type="number"
                 name="salary"
                 value={input.salary}
-                placeholder="*numeric only in L.P.A (INR)"
+                placeholder="*INR in Lacs per Year"
                 onChange={changeEventHandler}
                 className=" focus-visible:ring-0 focus-visible:ring-offset-0"
               />
@@ -154,10 +178,10 @@ const Postjobs = () => {
             <div>
               <Label>Experience</Label>
               <Input
-                type="text"
+                type="number"
                 name="experience"
                 value={input.experience}
-                placeholder="*numeric only in Years"
+                placeholder="Years of experience"
                 onChange={changeEventHandler}
                 className=" focus-visible:ring-0 focus-visible:ring-offset-0"
               />
@@ -172,39 +196,23 @@ const Postjobs = () => {
                 className=" focus-visible:ring-0 focus-visible:ring-offset-0"
               />
             </div>
-            {companies.length !== 0 && (
-              <Select onValueChange={selecteventHndler}>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder="*Select a Company" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    {companies.map((company) => {
-                      return (
-                        <SelectItem
-                          key={company._id}
-                          value={company?.name.toLowerCase()}
-                        >
-                          {company.name}
-                        </SelectItem>
-                      );
-                    })}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            )}
+            
           </div>
-          <Button type="submit" className="w-full mx-auto mt-5" disabled={companies.length === 0}>
-            {!loading ? "Post new job" : <Loader2 className="animate-spin" />}
+          <Button
+            type="submit"
+            className="w-full mt-6 bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2"
+            disabled={companies.length === 0 || loading}
+          >
+            {!loading ? "Post New Job" : <Loader2 className="animate-spin" />}
           </Button>
-
           {companies.length === 0 && (
-            <p className="text-xs text-red-600 font-bold mt-4 text-center">
-              *Please register a company first, before posting a job
+            <p className="text-center text-sm text-red-600 mt-4">
+              Please register a company before posting a job.
             </p>
           )}
         </form>
       </div>
+      <Footer/>
     </div>
   );
 };
